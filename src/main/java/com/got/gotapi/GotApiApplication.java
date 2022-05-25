ab48e4278fd1e708444c0005feaee3afe82000b8
paul.rus <paul.rus@globant.com>
2022-05-25 12:27:42 +0300

    Create project

---
+++ src/main/java/com/got/gotapi/GotApiApplication.java
@@ -0,0 +1,15 @@
+package com.got.gotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class GotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/got/gotapi/GotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.got.gotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
